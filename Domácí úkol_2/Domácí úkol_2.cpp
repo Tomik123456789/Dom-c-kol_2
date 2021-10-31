64c890e18b5eb91ad58f065fb24eb687777bbece
Uzivatel <Uzivatel@POTA>
2021-10-31 17:28:04 +0100

    Přidejte soubory projektu.

---
+++ Domácí úkol_2/Domácí úkol_2.cpp
@@ -0,0 +1,35 @@
+﻿
+
+#include <stdio.h>
+
+int main()
+{
+    // vypíšeme největší číslo
+    int a = 10000;
+    int b = 800;
+    int c = 11100;
+    int max;
+    if (a > b) {
+        // a je větší
+    if (a > c)
+        // a je větší
+        max = a;
+    else {
+        // c je větší
+        max = c;
+    }
+        }
+    else {
+        if (b > a); {
+            // b je větší
+            if (b > c)
+                // b je větší
+                max = b;
+            else {
+                // c je větší
+                max = c;
+        }
+        }
+    }
+        printf("nejvetsi je cislo %d", max);
+    }
